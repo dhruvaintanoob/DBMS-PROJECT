049429152d93eadbcf66131a79066d41cbadc90a
DhruvKothari <dhruvkotharihogwarts@gmail.com>
2026-02-13 14:43:40 +0530

    Resolved merge conflict in Content entity

--- src/main/java/com/dbms/netflix_clone/Entity/Content.java
+++ src/main/java/com/dbms/netflix_clone/Entity/Content.java
@@ -2,16 +2,30 @@ package com.dbms.netflix_clone.Entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+<<<<<<< Updated upstream
 import java.time.LocalDate; // for DATE types in java
 
 @Entity
 @Table(name = "content")
 @Data
 public class Content {
+=======
+
+
+
+
+@Entity
+@Data
+
+
+public class Content {
+
+>>>>>>> Stashed changes
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+<<<<<<< Updated upstream
     @Column(nullable = false)
     private String title;
     
@@ -25,4 +39,16 @@ public class Content {
 
     @Column(columnDefinition = "TEXT") // TEXT is a data type in MySQL for long text
     private String description;
+=======
+    
+    @Column(nullable =false);
+    private String title;
+
+    private String description;
+    private String genre;
+    private int releaseYear;
+    private double rating;
+    
+    
+>>>>>>> Stashed changes
 }
